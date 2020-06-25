d0c91b567c5e2e0a149a0fa140890a66a1c51552
Shane Mapasie <60961405+SPMapasie216182425@users.noreply.github.com>
2020-06-25 17:40:43 +0200

    I have added Pattern Builder but I did not add the methods that were part of the UML bro just clarify with me whether I should've added the methods before I used builder pattern.

--- src/main/java/za/ac/cput/elective/entity/Elective.java
+++ src/main/java/za/ac/cput/elective/entity/Elective.java
@@ -1,4 +1,30 @@
 package za.ac.cput.elective.entity;
 
 public class Elective {
+
+    private String electName;
+    private long electCode;
+    private String electDesc;
+
+
+    //Argument Constructor
+     Elective(String electName, long electCode, String electDesc) {
+        this.electName = electName;
+        this.electCode = electCode;
+        this.electDesc = electDesc;
+    }
+
+    @Override
+    public String toString() {
+        return "Elective{" +
+                "electName='" + electName + '\'' +
+                ", electCode=" + electCode +
+                ", electDesc='" + electDesc + '\'' +'}';
+    }
+
+    //start of main method
+    public static void main(String[]args){}
 }
+
+
+
