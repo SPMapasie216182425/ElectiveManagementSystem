23274efca808b80cd48f224a3148fb336ac6159d
Shane Mapasie <60961405+SPMapasie216182425@users.noreply.github.com>
2020-06-27 19:54:14 +0200

    I added the code to ELECTIVE according to the builder pattern design @William

--- src/main/java/za/ac/cput/elective/entity/Elective.java
+++ src/main/java/za/ac/cput/elective/entity/Elective.java
@@ -2,29 +2,89 @@ package za.ac.cput.elective.entity;
 
 public class Elective {
 
+    /* Encapsulation */
     private String electName;
     private long electCode;
     private String electDesc;
 
-
-    //Argument Constructor
-     Elective(String electName, long electCode, String electDesc) {
-        this.electName = electName;
-        this.electCode = electCode;
-        this.electDesc = electDesc;
+    /* constructor */
+    private Elective(Builder b) {
+        this.electName = b.electName;
+        this.electCode = b.electCode;
+        this.electDesc = b.electDesc;
+    }
+    //getters
+    public String getElectName() {
+        return electName;
     }
 
+    public long getElectCode() {
+        return electCode;
+    }
+
+    public String getElectDesc() {
+        return electDesc;
+    }
+    //toString Method
     @Override
     public String toString() {
         return "Elective{" +
-                "electName='" + electName + '\'' +
-                ", electCode=" + electCode +
-                ", electDesc='" + electDesc + '\'' +'}';
+                "Elective Name=" + electName +
+                ", Elective Code=" + electCode +
+                ", Elective Description='" + electDesc + '\'' +
+                '}';
     }
+    public static class Builder {
 
-    //start of main method
-    public static void main(String[]args){}
+        private String electName;
+        private long electCode;
+        private String electDesc;
+
+        /* setters using builder */
+        public Builder setElectName(String electName) {
+            this.electName = electName;
+            return this;
+        }
+
+        public Builder setElectCode(long electCode) {
+            this.electCode = electCode;
+            return this;
+        }
+
+        public Builder setElectDesc(String electDesc) {
+            this.electDesc = electDesc;
+            return this;
+        }
+
+        /*Constructor with Builder Implemented*/
+        public Builder copy(Elective elect) {
+            this.electName = elect.electName;
+            this.electCode = elect.electCode;
+            this.electDesc = elect.electDesc;
+            return this;
+        }
+
+        /*Methods*/
+        public Elective build() {
+            return new Elective(this);
+        }
+
+        public void viewElectStatus() {
+            //
+        }
+
+        public void viewElectLecture() {
+            //
+        }
+
+        public void checkSeatAvail() {
+            //
+        }
+
+        public void checkElectTypeOffered() {
+            //
+        }
+    }
 }
-
 
 
